3aebfe127462b81043187180fbea5d12f71cd0d0
Dinesh107 <dineshjr107@gmail.com>
2024-04-02 21:14:21 +0530

    first commit TCF

---
+++ index.js
@@ -0,0 +1,100 @@
+// console.log("Welcome To JS");
+
+// console.log("Welcome To JS");
+
+// console.log("Welcome To JS");
+
+// console.log("Welcome To JS");
+
+// console.log("Welcome To JS");
+
+// window.alert("Welcome To JS sathish");
+
+// below code is Variable
+
+// let a;  Decalarization
+
+// a = 12 initialization 
+ 
+
+// let a = 12; // Decalarization + initialization. 
+
+// console.log(a);
+
+// let firstName = "Sathish"; // strings
+
+// let age = 20;  // numbers
+
+// let student = true; //  booleans
+
+// age = age + 3;
+
+
+
+// console.log("Hello ", firstName);
+
+// console.log("Your age is ", age);
+
+// console.log("You have Enrolled:- ", student);
+
+
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+
+// document.getElementById("p2").innerHTML = "Your age is " + age;
+
+// document.getElementById("p3").innerHTML = "Enrolled:- " + student;
+
+//Arithemetic Expressions 
+
+// it is combination of operands (values, variables, etc), operators (+ - * / % modulus)
+
+// let a = 10;
+// let b = 20;
+
+// let c = a + b;
+// let d = b - a;
+// let e = b * a;
+// let f = b / a;
+// let g = b % a;
+
+
+
+let students = 20;
+
+// students = students + 1;
+
+// students = students - 2;
+
+// students = students * 2;
+
+// students = students / 2;
+
+// students = students % 2;
+
+students += 1; // agumented assignment operator
+
+students -= 1;
+
+students *= 1;
+
+students /= 1;
+
+students %= 1;
+
+console.log(students);
+
+
+
+
+// console.log(c);
+
+// console.log(d);
+
+// console.log(e);
+
+// console.log(f);
+
+// console.log(g);
+
+
+
